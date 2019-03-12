d34689f59470dabcdfd747098c7b87a91acb0eac
Noah Flint <noah.flint24@gmail.com>
2019-03-12 17:30:54 -0600

    created model test for comedians unique cities

--- spec/models/comedian_spec.rb
+++ spec/models/comedian_spec.rb
@@ -4,7 +4,7 @@ describe Comedian, type: :model do
   before :each do
     @comedian_1 = Comedian.create!(name: "Comedian_1", age: "30", city: "City_1")
     @comedian_2 = Comedian.create(name: "Comedian_2", age: "50", city: "City_2")
-    @comedian_3 = Comedian.create(name: "Comedian_3", age: "100", city: "City_3")
+    @comedian_3 = Comedian.create(name: "Comedian_3", age: "100", city: "City_2")
     @special_1 = @comedian_1.specials.create( title: "Title 1", runtime: 100, thumbnail: "http://ingstadmedia.com/kduz/wp-content/uploads/sites/7/2018/08/microphone-clipart-1.jpg")
     @special_2 = @comedian_1.specials.create(title: "Title 2", runtime: 150, thumbnail: "http://ingstadmedia.com/kduz/wp-content/uploads/sites/7/2018/08/microphone-clipart-1.jpg")
     @special_3 = @comedian_2.specials.create(title: "Title 3", runtime: 50, thumbnail: "http://ingstadmedia.com/kduz/wp-content/uploads/sites/7/2018/08/microphone-clipart-1.jpg")
@@ -27,7 +27,11 @@ describe Comedian, type: :model do
 
       expect(actual).to eq(60)
     end
-
-
+    # it '.cities' do
+    #   actual = Comedian.cities
+    #   expected = ["City_1", "City_2"]
+    #
+    #   expect(actual).to eq(expected)
+    # end
   end
 end
