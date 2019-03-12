708f867f33a69d30978e79707367ea91d69be688
Noah Flint <noah.flint24@gmail.com>
2019-03-12 15:27:29 -0600

    created model test for average age class method

--- spec/models/comedian_spec.rb
+++ spec/models/comedian_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
 describe Comedian, type: :model do
+  before :each do
+    @comedian_1 = Comedian.create!(name: "Comedian_1", age: "30", city: "City_1")
+    @comedian_2 = Comedian.create(name: "Comedian_2", age: "50", city: "City_2")
+    @comedian_3 = Comedian.create(name: "Comedian_3", age: "100", city: "City_3")
+    @special_1 = @comedian_1.specials.create( title: "Title 1", runtime: 100, thumbnail: "http://ingstadmedia.com/kduz/wp-content/uploads/sites/7/2018/08/microphone-clipart-1.jpg")
+    @special_2 = @comedian_1.specials.create(title: "Title 2", runtime: 150, thumbnail: "http://ingstadmedia.com/kduz/wp-content/uploads/sites/7/2018/08/microphone-clipart-1.jpg")
+    @special_3 = @comedian_2.specials.create(title: "Title 3", runtime: 50, thumbnail: "http://ingstadmedia.com/kduz/wp-content/uploads/sites/7/2018/08/microphone-clipart-1.jpg")
+    @special_4 = @comedian_3.specials.create(title: "Title 4", runtime: 10, thumbnail: "http://ingstadmedia.com/kduz/wp-content/uploads/sites/7/2018/08/microphone-clipart-1.jpg")
+  end
+
   describe 'Validations' do
     it { should validate_presence_of :name }
     it { should validate_presence_of :age }
@@ -9,5 +19,15 @@ describe Comedian, type: :model do
 
   describe 'Relationships' do
     it { should have_many :specials}
+  end
+
+  describe 'Class Methods' do
+    it '.average_age' do
+      actual = Comedian.average_age
+
+      expect(actual).to eq(60)
+    end
+
+
   end
 end
