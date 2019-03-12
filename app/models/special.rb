69f8625c77d922e9a08258a5abbc5eb648f996fd
Noah Flint <noah.flint24@gmail.com>
2019-03-12 16:19:33 -0600

    created average length method for special

--- app/models/special.rb
+++ app/models/special.rb
@@ -2,4 +2,8 @@ class Special < ApplicationRecord
     validates_presence_of :title, :runtime, :thumbnail
 
     belongs_to :comedian
+
+    def self.average_length
+      Special.average(:runtime)
+    end
 end
--- spec/models/special_spec.rb
+++ spec/models/special_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
 describe Special, type: :model do
+  before :each do
+    @comedian_1 = Comedian.create!(name: "Comedian_1", age: "30", city: "City_1")
+    @comedian_2 = Comedian.create(name: "Comedian_2", age: "50", city: "City_2")
+    @comedian_3 = Comedian.create(name: "Comedian_3", age: "100", city: "City_3")
+    @special_1 = @comedian_1.specials.create( title: "Title 1", runtime: 100, thumbnail: "http://ingstadmedia.com/kduz/wp-content/uploads/sites/7/2018/08/microphone-clipart-1.jpg")
+    @special_2 = @comedian_1.specials.create(title: "Title 2", runtime: 150, thumbnail: "http://ingstadmedia.com/kduz/wp-content/uploads/sites/7/2018/08/microphone-clipart-1.jpg")
+    @special_3 = @comedian_2.specials.create(title: "Title 3", runtime: 50, thumbnail: "http://ingstadmedia.com/kduz/wp-content/uploads/sites/7/2018/08/microphone-clipart-1.jpg")
+    @special_4 = @comedian_3.specials.create(title: "Title 4", runtime: 10, thumbnail: "http://ingstadmedia.com/kduz/wp-content/uploads/sites/7/2018/08/microphone-clipart-1.jpg")
+  end
+
     describe 'Validations' do
         it { should validate_presence_of :title}
         it { should validate_presence_of :runtime}
@@ -15,7 +25,7 @@ describe Special, type: :model do
       it '.average_length' do
         actual = Special.average_length
 
-        expect(actual).to eq(100)
+        expect(actual).to eq(77.5)
       end
 
     end
